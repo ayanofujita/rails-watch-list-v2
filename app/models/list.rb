6c7e1310037d8ae2d72eb0056c25365fb7dcb36d
Ayano Fujita <cathy.ayano@gmail.com>
2024-10-09 23:06:03 +0900

    poster group effect

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,9 +5,13 @@ class List < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 55 }
   validates :description, length: { maximum: 400 }
-  # validate :has_atleast_5
+  validate :has_atleast_5
 
   def has_atleast_5
     errors.add(:movies, "must have at least 5 movies") if movies.size < 5
+  end
+
+  def first_5
+    self.movies.first(5)
   end
 end
