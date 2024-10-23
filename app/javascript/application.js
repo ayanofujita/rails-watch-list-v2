4ce752e22903c18c7793db55f2f1c11a5b142635
Ayano Fujita <cathy.ayano@gmail.com>
2024-10-23 18:41:43 +0900

    sidebar new display for mobile

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,20 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+
+window.addEventListener("resize", hideSidebar);
+function hideSidebar() {
+  if (window.innerWidth <= 576){
+    document.querySelector('.sidebar').childNodes.forEach((element) => {
+      if (element.localName == "div"){
+        element.classList.add('d-none')
+      }
+    })
+  } else {
+    document.querySelector('.sidebar').childNodes.forEach((element) => {
+      if (element.localName == "div"){
+        element.classList.remove('d-none')
+      }
+    })
+  }
+}
