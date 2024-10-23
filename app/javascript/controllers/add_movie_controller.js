15f637b9c529737adbc30a9127c46196f48c9b2a
Ayano Fujita <cathy.ayano@gmail.com>
2024-10-23 16:31:25 +0900

    move sass files

--- app/javascript/controllers/add_movie_controller.js
+++ app/javascript/controllers/add_movie_controller.js
@@ -10,10 +10,10 @@ export default class extends Controller {
     const movie_poster = this.element.dataset.url
     const img = `
     <div class="me-2 mb-2">
+      <img src="${movie_poster}" alt="" class="movie-poster">
       <button type="button" class="text-white" aria-label="Remove" data-controller="remove-poster" data-action="click->remove-poster#remove">
         <i class="fa-solid fa-xmark"></i>
       </button>
-      <img src="${movie_poster}" alt="" class="movie-poster">
       <input type="hidden" name="list[movies][]" value="${movie_imdb}">
     <div>
     `
